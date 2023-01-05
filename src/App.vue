b537d75bb8a9b4f4be42da80d68485b14f04836b
Dan Hall <dhall0803@gmail.com>
2023-01-05 19:09:36 +0000

    Add rest of files

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import StoneToLbs from './components/StoneToLbs.vue'
+</script>
+
+<template>
+  <header>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item">
+            Home
+          </a>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <div class="section">
+    <div class="container">
+      <StoneToLbs></StoneToLbs>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/StoneToLbs.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { ref } from 'vue'
+
+const inputStones = ref(0)
+const inputPounds = ref(0.0)
+const outputPounds = computed(() => {
+  return inputStones.value * 14 + inputPounds.value
+}) 
+
+</script>
+
+<template>
+    <h1>Stone and Pounds to Pounds</h1>
+    <div class="field">
+        <label class="label">Stones</label>
+        <div class="control">
+            <input v-model="inputStones" class="input" type="number">
+        </div>
+    </div>
+    <div class="field">
+        <label class="label">Pounds</label>
+        <div class="control">
+            <input v-model="inputPounds" class="input" type="number" step="0.1" max=13.9>
+        </div>
+    </div>
+    <div class="field">
+        <label class="label">Total Pounds</label>
+        <div class="control">
+            <input v-model="outputPounds" class="input" type="number" readonly>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
